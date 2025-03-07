00f8a010fd1c6118daab0ad5751fccc5b9c1d81e
Yeshwanth <157149527+Yeshwanth0124@users.noreply.github.com>
2025-03-07 23:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def data():
      except Exception as e:
          return jsonify({"error":str(e)})'''
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,use_reloader=False)
